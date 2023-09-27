bf359a2ff682ebf274cd82183ea74e51e0da8f07
Alumno <Alumno@LAB1504-02>
2023-09-26 21:59:58 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp1/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Clientes
+    {
+        public int idCliente { get; set; }
+        public int NombreCompañia { get; set; }
+        public int NombreContacto { get; set; }
+        public int CargoContacto { get; set; }
+        public int Direccion { get; set; }
+        public int Ciudad { get; set; }
+        public int Region { get; set; }
+        public int CodPostal { get; set; }
+        public int Pais { get; set; }
+        public int Telefono { get; set; }
+        public int Fax { get; set; }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string connectionString = "Data Source=LAB1504-02\\SQLEXPRESS;Initial Catalog=Neptuno5;User ID=userTecsup;Password=123456";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("InsertarClientes", connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@idCliente", (txtidCliente.Text));
+                    cmd.Parameters.AddWithValue("@NombreCompañia", txtNombreCompañia.Text);
+                    cmd.Parameters.AddWithValue("@NombreCompañia", txtNombreContacto.Text);
+                    cmd.Parameters.AddWithValue("@NombreContacto", txtCargoContacto.Text);
+                    cmd.Parameters.AddWithValue("@CargoContacto", txtCiudad.Text);
+                    cmd.Parameters.AddWithValue("@Region", txtRegion.Text);
+                    cmd.Parameters.AddWithValue("@CodPostal", txtCodPostal.Text);
+                    cmd.Parameters.AddWithValue("@Pais", txtPais.Text);
+                    cmd.Parameters.AddWithValue("@Telefono", txtTelefono.Text);
+                    cmd.Parameters.AddWithValue("@Fax", txtFax.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Categoría ingresada correctamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al ingresar la categoría: " + ex.Message);
+            }
+        }
+    }
+    
+}
